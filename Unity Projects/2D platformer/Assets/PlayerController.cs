19eb6738319f7138f6c7ffd0e94cfcd32ef0d4c4
genaralskar <genaralskar1@gmail.com>
2019-10-25 20:49:02 -0600

    Level 1 and the stuff needed for it, tractor beam

--- Unity Projects/2D platformer/Assets/PlayerController.cs
+++ Unity Projects/2D platformer/Assets/PlayerController.cs
@@ -6,9 +6,12 @@ using UnityEngine.Events;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private CircleCollider2D col;
+    
     [Header("Movement")]
     public float moveSpeed = 2f;
     public float jumpForce = 3f;
+    public bool canMove = true;
     //public float jumpTimeForgivness = .2f;
     public int jumpAmount = 1;
     public int maxJumps = 1;
@@ -18,10 +21,8 @@ public class PlayerController : MonoBehaviour
     public ContactFilter2D cFilter;
     public bool useGravity = true;
 
-    private Vector2 groundedRayOffset { get { return Gravity.down * .5f; } }
+    private Vector2 groundedRayOffset { get { return Gravity.down * transform.localScale.x * col.radius; } }
     private bool isGrounded = false;
-
-    public GravityManager gravMan;
     
     [Header("Fast Fall")]
     public float fastFallForce = 1;
@@ -51,6 +52,7 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<CircleCollider2D>();
         xDrag = 1 - xDrag;
         yDrag = 1 - yDrag;
         airDragMod = 1 - airDragMod;
@@ -59,21 +61,26 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        DoGravity();
+        if(useGravity)
+            DoGravity();
         SetIsGrounded();
-        Move();
-        FastFall();
-        Drag();
+        if (canMove)
+        {
+            Move();
+            FastFall();
+            Drag();
+        }
     }
 
     private void Update()
     {
-        Jump();
+        if(canMove)
+            Jump();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.relativeVelocity.magnitude > 5f)
+        if (other.relativeVelocity.magnitude > 3f)
         {
             impactSound.Play();
         }
@@ -108,7 +115,8 @@ public class PlayerController : MonoBehaviour
     private bool IsGrounded()
     {
         Vector2 currentPos = transform.position;
-        RaycastHit2D hit = Physics2D.Raycast(currentPos + groundedRayOffset, Gravity.down, groundedDistance,
+        float scale = transform.localScale.x;
+        RaycastHit2D hit = Physics2D.Raycast((currentPos + groundedRayOffset), Gravity.down, groundedDistance * scale,
             groundedLayers, 0, 0);
         
         if (hit && hit.distance > groundedDistance * 0.1f)
@@ -140,7 +148,8 @@ public class PlayerController : MonoBehaviour
         
         //inversedirection or whatever of current gravity
         //Debug.Log(transform.InverseTransformDirection(Gravity.gravity));
-        Vector2 vel = gravMan.transform.InverseTransformDirection(rb.velocity);
+        if (!useGravity) return;
+        Vector2 vel = rb.velocity;
         
         if (vel.y < fastFallThreshold)
         {
@@ -151,12 +160,9 @@ public class PlayerController : MonoBehaviour
     private void Drag()
     {
         Vector2 vel = rb.velocity;
-        vel = gravMan.transform.InverseTransformDirection(vel);
-        Vector2 test = gravMan.transform.TransformDirection(vel);
-        Vector2 test2 = transform.TransformDirection(Gravity.down);
         Debug.DrawRay(transform.position, vel, Color.red);
-        Debug.DrawRay(transform.position, test, Color.magenta);
-        Debug.DrawRay(transform.position, test2, Color.cyan);
+//        Debug.DrawRay(transform.position, test, Color.magenta);
+//        Debug.DrawRay(transform.position, test2, Color.cyan);
         //Debug.Log(vel);
 
         if (isGrounded)
---
+++ Unity Projects/2D platformer/Assets/TractorBeam.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.XR;
+using Debug = UnityEngine.Debug;
+
+public class TractorBeam : MonoBehaviour
+{
+    public Vector2 tractorPoint;
+    public float tractorForce = 5f;
+    public GameObject endGameCam;
+    public float tractorMoveSpeed = 4f;
+
+    private Transform playerTransform;
+    private Rigidbody2D rb;
+    private bool tractoring = false;
+    private Vector2 tractorPointTarget;
+    
+    private Vector2 position {get {return new Vector2(transform.position.x, transform.position.y);}}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (tractoring) return;
+        PlayerController controller = other.GetComponent<PlayerController>();
+        controller.canMove = false;
+        controller.useGravity = false;
+        playerTransform = controller.transform;
+        rb = controller.gameObject.GetComponent<Rigidbody2D>();
+
+        rb.drag = 1f;
+        rb.angularDrag = 1f;
+
+        tractorPointTarget = tractorPoint;
+        tractorPoint.y = playerTransform.position.y - position.y;
+
+        StopAllCoroutines();
+        StartCoroutine(Tractor());
+        StartCoroutine(MoveTractorPoint());
+        endGameCam.SetActive(true);
+    }
+
+
+    private IEnumerator Tractor()
+    {
+        tractoring = true;
+        
+
+        while (true)
+        {
+            Vector2 playerPos = playerTransform.position;
+            Vector2 dir = playerPos - (tractorPoint + position);
+            Debug.DrawRay(playerPos, -dir);
+            //probably normalize this
+
+            float dirMag = dir.magnitude;
+            dir = dir.normalized;
+            
+            rb.AddForce(-dir * tractorForce);
+            
+            
+            
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    private IEnumerator MoveTractorPoint()
+    {
+        while (Mathf.Abs(tractorPoint.y - tractorPointTarget.y) > .1f)
+        {
+            Debug.Log("moving on up?");
+            //move tractor point
+            float dirMag = Vector2.Distance(tractorPoint + position, playerTransform.position);
+            Debug.Log(dirMag);
+            if (dirMag < 2f)
+            {
+                Debug.Log("Upward movement!");
+                tractorPoint.y += tractorMoveSpeed * Time.deltaTime;
+            }
+            
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        
+        Gizmos.DrawWireSphere(tractorPoint + position, .1f);
+    }
+}
