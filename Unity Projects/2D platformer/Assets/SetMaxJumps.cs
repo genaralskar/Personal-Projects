c95df85ce6e6e54e9b42dfb124771313950ec96d
Collin <genaralskar1@gmail.com>
2019-10-27 19:29:49 -0600

    Level 2 layout done

---
+++ Unity Projects/2D platformer/Assets/SetMaxJumps.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMaxJumps : Pickup
+{
+    public int newMaxJumps = 1;
+    
+    protected override void OnPickUp(PlayerController player)
+    {
+        player.maxJumps = newMaxJumps;
+        player.jumpAmount += newMaxJumps;
+    }
+    
+}
