670e280a8ab1e254070e683930b2d3a7b955d0b3
genaralskar <genaralskar1@gmail.com>
2020-01-19 13:02:12 -0700

    change gitignore because it was ignoring files starting with Build

---
+++ Unity Projects/Build-A-House/Assets/Scripts/Build/BuildManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BuildManager : MonoBehaviour
+{
+    public GameObject building;
+    public Buildable buildable;
+    public GameObject buildCam;
+    public LayerMask layerMask;
+
+    public static bool isBuilding;
+
+    private void Start()
+    {
+        //StartBuilding(building);
+    }
+
+    public void StartBuilding(GameObject newBuilding)
+    {
+        if (newBuilding == null) return;
+
+        building = Instantiate(newBuilding);
+        buildable = building.GetComponent<Buildable>();
+        isBuilding = true;
+
+        StartCoroutine(PlaceBuilding());
+    }
+
+    private IEnumerator PlaceBuilding()
+    {  
+        buildCam.SetActive(true);
+        buildable.StartBuilding();
+        while (isBuilding)
+        {
+
+            if (EventSystem.current.IsPointerOverGameObject(-1)) continue;
+
+            if (Input.GetMouseButton(1))
+            {
+                Destroy(building);
+                break;
+            }
+
+
+            //raycast
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+            {
+                building.transform.position = hit.point;
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                buildable.PlaceBuilding();
+                building = null;
+                buildable = null;
+                break;
+            }
+
+            yield return null;
+        }
+
+        isBuilding = false;
+        buildCam.SetActive(false);
+    }
+}
---
+++ Unity Projects/Build-A-House/Assets/Scripts/Build/Buildable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buildable : MonoBehaviour
+{
+    public List<Collider> colliders;
+
+    public void StartBuilding()
+    {
+        foreach (var col in colliders)
+        {
+            col.enabled = false;
+        }
+        
+    }
+    
+    public void PlaceBuilding()
+    {
+        foreach (var col in colliders)
+        {
+            col.enabled = true;
+        }
+    }
+    
+}
